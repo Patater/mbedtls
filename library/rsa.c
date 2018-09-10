Patater/mbedtls

commit 0287ad9165c0c4a6de083aedde07c4d5085b0dc3

Update RSA PKCS#1 V2.1 sign function to be compliant with FIPS-186-4

library/rsa.c
@@ -1550,7 +1550,12 @@ int mbedtls_rsa_rsassa_pss_sign( mbedtls_rsa_context *ctx,
         return( MBEDTLS_ERR_RSA_BAD_INPUT_DATA );
 
     hlen = mbedtls_md_get_size( md_info );
-    slen = hlen;
+
+    /* Align with FIPS-186-4 */
+    if ( olen == 128 && hlen == 64 )
+        slen = hlen - 2;
+    else
+        slen = hlen;
 
     if( olen < hlen + slen + 2 )
         return( MBEDTLS_ERR_RSA_BAD_INPUT_DATA );

commit 7fb7f910de632ba5fc5c087f7766fd8db3092f80

rsa: pss: Correctly calculate start of salt
Now that RSASSA-PSS signatures can use a salt length smaller than the
hash length, we must calculate the start of the salt taking into account
the potentially smaller slen (no longer assuming that slen == hlen).

Fixes: 0287ad9165c0 ("Update RSA PKCS#1 V2.1 sign function to be compliant with FIPS-186-4")

library/rsa.c
@@ -1571,7 +1571,7 @@ int mbedtls_rsa_rsassa_pss_sign( mbedtls_rsa_context *ctx,
 
     /* Note: EMSA-PSS encoding is over the length of N - 1 bits */
     msb = mbedtls_mpi_bitlen( &ctx->N ) - 1;
-    p += olen - hlen * 2 - 2;
+    p += olen - hlen - slen - 2;
     *p++ = 0x01;
     memcpy( p, salt, slen );
     p += slen;